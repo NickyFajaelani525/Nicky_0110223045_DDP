6b5cf57b687b2929792bc61e6f35e5a22d35dc61
NickyFajaelani525 <nickyfajaelani@gmail.com>
2023-10-31 20:50:49 -0700

    praktik6

---
+++ tes6.py
@@ -0,0 +1,23 @@
+for i in range(1,8):
+      print(i)
+      
+for i in range(50):
+      print(i, end="")
+#keatas
+for i in range(0,50,5):
+      print(i) 
+#kebawah
+for i in range(50,0,-5):
+      print(i)  
+
+#print kata
+for i in range(0,4):
+      print("ujang") 
+
+#for bertingkat
+for i in range(1,11):
+      for j in range(1,11):
+            k=i*j
+            print(k,end='')
+      print()
+
